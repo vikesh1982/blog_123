ac2cefef4098628efe19db4f50016e406d7eecce
pramasamy <ponswaminathan.ramasamy@pnmac.com>
2014-01-22 14:08:21 -0800

    Complete CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,21 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+
   def create
     @post = Post.new(post_params)
     if @post.save
@@ -19,6 +34,14 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    @post.destroy
+
+    redirect_to posts_path
   end
 
   private
